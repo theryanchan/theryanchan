8e0edefa822be5f59020bda66eae00a15be573b1
Ryan Chan <42970967+theryanchan@users.noreply.github.com>
2020-09-06 02:54:37 -0400

    Pushed out Simon

---
+++ Simon-Game/game.js
@@ -0,0 +1,113 @@
+//variable declarations
+
+var buttonColors = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
+
+
+var started = false;
+var gameIndex = 0;
+
+
+
+//listens for a keydown to start the game
+$(document).on("keydown", function(){
+  if (!started){
+    $("h1").text("Level 1");
+    setTimeout(function(){nextSequence()}, 800);
+
+    started=true;
+  }
+});
+
+
+
+//event that triggers when a button is clicked. also adds to userClickedPattern
+$(".btn").click(function(event){
+  var userChosenColor = $(this).attr("id");
+  flashAndPlay(userChosenColor);
+  userClickedPattern.push(userChosenColor);
+  console.log(userChosenColor);
+  animatePress(userChosenColor);
+  checkAnswer(userClickedPattern.length-1);
+});
+
+//added animation to a clicked button
+function animatePress(currentColor){
+  $("#" + currentColor).addClass("pressed");
+  setTimeout(function(){
+  $("#" + currentColor).removeClass("pressed");
+}, 200);
+}
+
+
+
+//Check Answer
+function checkAnswer(currentLevel){
+
+  if (userClickedPattern[currentLevel] === gamePattern[currentLevel]){
+    if(userClickedPattern.length === gamePattern.length){
+      console.log("success");
+
+      setTimeout(function(){
+        nextSequence();
+      }, 1000);
+    }
+  }
+  else {
+        $("body").addClass("game-over");
+        playColorSound("wrong");
+        $("h1").text("Game Over, Press Any Key To Restart")
+        restart();
+    setTimeout(function(){
+          $("body").removeClass("game-over");
+    },200);
+
+
+    console.log("wrong");
+  }
+}
+
+
+
+
+//RNG between 0-3, adds new color to gamePattern, and plays/flashes correct button
+function nextSequence() {
+  userClickedPattern = [];
+  var randomNumber = Math.floor(Math.random() * 4);
+
+  var randomChosenColor = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColor);
+  gameIndex++;
+  $("h1").text("Level " + gameIndex);
+
+  flashAndPlay(randomChosenColor);
+
+}
+
+function restart(){
+  gameIndex = 0;
+  gamePattern = [];
+  started = false;
+}
+
+
+//function that flashes buttons and plays sound of color passed in
+function flashAndPlay(color){
+  flashColor(color);
+  playColorSound(color);
+}
+
+//function that flashes a button with color passed in
+function flashColor(color){
+    // $("#" + randomChosenColor).fadeOut(100).fadeIn(100);//another way of flashing the button
+  $("#" + color)
+    .animate({opacity : 0.4}, 200)
+    .animate({opacity : 1}, 200);
+};
+
+// function that plays the mp3 of the color passed in
+function playColorSound(color) {
+    var audio = new Audio("sounds/" + color + ".mp3");
+    audio.play();
+};
--- jQuery/index.js
+++ jQuery/index.js
@@ -1,1 +1,54 @@
-$("h1").css("color","red");
+// set values for element :
+        // $("h1").css("color","red");
+
+// get values for element :
+        // console.log($("h1").css("color"));
+
+// set or add a class :
+        // $("h1").addClass("big-title");
+
+// adding a class :
+        // $("h1").addClass("big-title"); // to add multiple classes, add $("h1").addClass("class0 class1 class2");
+// check an element for a class - can also use the .hasClass("____"); function to return a true or false boolean.
+
+// change the text to another word or phrase :
+        // $("h1").text("Bye");
+
+// add inner HTML :
+        //$("button").html("<em>Hey</em>"); //add innerHTML method;
+// adding a clickable event :
+        // $("h1").click(function() {
+        //   $("h1").css("color", "#faa2ff");
+        // });
+
+// super annoying keyboard tracker
+        // $("h1").addClass("big-title");
+        // $(document).keydown(function(event){
+        //   $("h1").text(event.key);
+        // });
+// using the .on function for listeners
+        // $("h1").on("mouseover", function(){
+        //   $("h1").css("color","purple");
+        // });//turns h1 purple when mouse hovers over it
+        // $("h1").on("mouseout", function(){
+        //   $("h1").css("color","black");
+        // });//turns h1 black when mouse moves off the title
+// add elements before and after the selected element.
+        // $("h1").before("<button>before Button</button>");
+        // $("h1").prepend("<button>prepend Button</button>");
+        // $("h1").after("<button>after Button</button>");
+        // $("h1").append("<button>append Button</button>");
+// animations such as hide, show, toggle, fade,
+// $("button").on("click", function(){
+        //   $("h1").hide();
+        //   $("h1").show();
+        //   $("h1").toggle();
+        //   $("h1").fadeOut();
+        //   $("h1").fadeIn();
+        //   $("h1").fadeToggle();
+        //   $("h1").slideUp();
+        //   $("h1").slideDown();
+        //   $("h1").slideToggle();
+        //   $("h1").animate({opacity: 0.5}); // in the animate css you can only add numerical values. you cannot do "red" for example.
+        //   $("h1").slideUp().slideDown().animate({margin: 40});// chained animations
+        // });
